db4e58a3d4e0a42228fc8fe6896631cd21903aa5
ugit5245 <umex.orque2@gmail.com>
2024-10-04 19:32:35 +0900

    kadai_007作成

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>PHP基礎編</title>
+</head>
+<body>
+<p>
+
+<?php
+
+$as_array = ['name'=>'onion','price'=>'30','weight'=>'160'];
+
+print_r($as_array);
+
+?>
+
+</p>
+</body>
+</html>
